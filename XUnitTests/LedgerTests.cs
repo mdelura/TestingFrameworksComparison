c6558bfd7ec1afe8fbbba60e788431b62cd0963a
Michał Delura <deluramichal@gmail.com>
2023-03-18 18:52:45 +0100

    Add XUnit tests

---
+++ XUnitTests/LedgerTests.cs
@@ -0,0 +1,118 @@
+using TestingFrameworksComparison;
+
+namespace XUnitTests;
+
+public class LedgerTests
+{
+    private Ledger _sut = new Ledger();
+
+    [Fact]
+    public void Increment_ShouldIncrementTheRequestedAmount()
+    {
+        // Arrange
+        const int amount = 3;
+
+        // Act
+        _sut.Increment(amount);
+
+        // Assert
+        Assert.Equal(amount, _sut.Amount);
+    }
+
+    [Fact]
+    public async Task CheckCurrentAmount_ShouldReturnCurrentAmount()
+    {
+        // Arrange
+        const int amount = 3;
+        const int incrementCount = 1000;
+
+        for (int i = 0; i < incrementCount; i++)
+        {
+            _sut.Increment(amount);
+        }
+
+        var expectedResult = amount * incrementCount;
+
+        // Act
+        var result = await _sut.CheckCurrentAmmount();
+
+
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+
+    [Fact]
+    public void Increment_ShouldIncrementTheRequestedAmount2()
+    {
+        // Arrange
+        const int amount = 3;
+
+        // Act
+        _sut.Increment(amount);
+
+        // Assert
+        Assert.Equal(amount, _sut.Amount);
+    }
+
+    [Fact]
+    public async Task CheckCurrentAmount_ShouldReturnCurrentAmount2()
+    {
+        // Arrange
+        const int amount = 3;
+        const int incrementCount = 1000;
+
+        for (int i = 0; i < incrementCount; i++)
+        {
+            _sut.Increment(amount);
+        }
+
+        var expectedResult = amount * incrementCount;
+
+        // Act
+        var result = await _sut.CheckCurrentAmmount();
+
+
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+
+    [Fact]
+    public void Increment_ShouldIncrementTheRequestedAmount3()
+    {
+        // Arrange
+        const int amount = 3;
+
+        // Act
+        _sut.Increment(amount);
+
+        // Assert
+        Assert.Equal(amount, _sut.Amount);
+    }
+
+    [Fact]
+    public async Task CheckCurrentAmount_ShouldReturnCurrentAmount3()
+    {
+        // Arrange
+        const int amount = 3;
+        const int incrementCount = 1000;
+
+        for (int i = 0; i < incrementCount; i++)
+        {
+            _sut.Increment(amount);
+        }
+
+        var expectedResult = amount * incrementCount;
+
+        // Act
+        var result = await _sut.CheckCurrentAmmount();
+
+
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
